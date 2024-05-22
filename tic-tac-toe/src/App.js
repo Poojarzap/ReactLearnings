eb5799db1549b40e59a56807e8dac288f72dd9a2
Poojarzap <[pooja.r@zapcg.com>
2024-05-22 16:12:54 +0530

    Created an interactive square component

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,25 +1,39 @@
 
+import { useState } from 'react';
 import './App.css';
 
-function Square() {
+function Square({ value, onClick}) {
+  return <button className='square' onClick={onClick}>{value}</button>
+}
+
+function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
+  console.log(squares)
   return (
-    <>
-    <div className='board-row'>
-      <button className='square'>1</button>
-      <button className='square'>2</button>
-      <button className='square'>3</button>
+    <div>
+      <div className='board-row'>
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className='board-row'>
-      <button className='square'>4</button>
-      <button className='square'>5</button>
-      <button className='square'>6</button>
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className='board-row'>
-      <button className='square'>7</button>
-      <button className='square'>8</button>
-      <button className='square'>9</button>
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
-    </>
+    </div>
   )
 
 }
@@ -27,7 +41,7 @@ function Square() {
 function App() {
   return (
     <>
-      <Square />
+      <Board />
     </>
   );
 }
